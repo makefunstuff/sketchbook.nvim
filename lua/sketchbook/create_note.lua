9ac38034f4153f68f93fe9ae75a9be70874c041b
iurii plugatarov <plugatariov@gmail.com>
2024-08-05 16:27:19 +0300

    prompt file instead

--- lua/sketchbook/create_note.lua
+++ lua/sketchbook/create_note.lua
@@ -2,8 +2,8 @@ local M = {}
 
 function M.create_new_note()
 	local notes_dir = vim.g.notes_directory or "~/notes/"
-	local filename = os.date("%Y-%m-%d_%H-%M-%S") .. ".md"
-	local filepath = vim.fn.expand(notes_dir) .. filename
+	local filename = vim.fn.input("File: ", "", "file")
+	local filepath = vim.fn.expand(notes_dir) .. "/" .. filename .. ".md"
 	local title = "Title " .. os.date("%Y-%m-%d %H:%M:%S")
 	local template = {
 		"# " .. title,
